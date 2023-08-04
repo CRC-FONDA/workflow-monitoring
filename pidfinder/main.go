63346a855617f32069295edb219273a5b21be3da
Soeren Becker <soeren.becker@tu-berlin.de>
2023-08-04 14:03:57 +0200

    Updated pidfinder; ran first experiments

--- pidfinder/main.go
+++ pidfinder/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 	"time"
 
@@ -22,9 +23,11 @@ type PodLabelsInfo struct {
 }
 
 type RunningContainer struct {
-	Pid     int
-	Created string
-	Info    PodLabelsInfo
+	Pid       int
+	Created   string
+	StartedAt string
+	Image     string
+	Info      PodLabelsInfo
 }
 
 var RESULT_PATH = ""
@@ -36,17 +39,17 @@ func parseContainerLabels(labels map[string]string) PodLabelsInfo {
 	podLabels.PodName = labels["io.kubernetes.pod.name"]
 	podLabels.ContainerName = labels["io.kubernetes.container.name"]
 	podLabels.PodNamespace = labels["io.kubernetes.pod.namespace"]
-	podLabels.App = labels["app"]
-	podLabels.NextflowRunName = labels["runName"]
-	podLabels.NextflowProcessName = labels["processName"]
-	podLabels.NextflowSessionID = labels["sessionId"]
-	podLabels.NextflowTaskName = labels["taskName"]
+	podLabels.App = labels["nextflow.io/app"]
+	podLabels.NextflowRunName = labels["nextflow.io/runName"]
+	podLabels.NextflowProcessName = labels["nextflow.io/processName"]
+	podLabels.NextflowSessionID = labels["nextflow.io/sessionId"]
+	podLabels.NextflowTaskName = labels["nextflow.io/taskName"]
 	return podLabels
 }
 
 func contains(l []*RunningContainer, c RunningContainer) bool {
 	for _, con := range l {
-		if con.Pid == c.Pid {
+		if con.Pid == c.Pid && con.Info.PodName == c.Info.PodName && con.Info.ContainerName == c.Info.ContainerName {
 			return true
 		}
 	}
@@ -64,24 +67,49 @@ func main() {
 		panic(err)
 	}
 
-	for {
-		newContainer := false
-		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	runningContainers := []*RunningContainer{}
+	// check if a result file already exists
+	if _, err := os.Stat(resultFile); err == nil {
+		oldResults, err := os.OpenFile(resultFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
 		if err != nil {
 			panic(err)
 		}
+		defer oldResults.Close()
 
-		runningContainers := []*RunningContainer{}
+		if err := gocsv.UnmarshalFile(oldResults, &runningContainers); err != nil {
+			panic(err)
+		}
+	}
+
+	for {
+		newContainer := false
+		containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{
+			All: true,
+		})
+
+		if err != nil {
+			fmt.Println("Error listing containers: ", err)
+			continue
+		}
+
 		for _, container := range containers {
 			containerJson, err := cli.ContainerInspect(context.Background(), container.ID)
 			if err != nil {
-				panic(err)
+				fmt.Println("Error inspecting container: ", err)
+				continue
+			}
+
+			// only store running containers
+			if !containerJson.State.Running {
+				continue
 			}
 
 			c := RunningContainer{
-				Pid:     containerJson.State.Pid,
-				Created: containerJson.Created,
-				Info:    parseContainerLabels(containerJson.Config.Labels),
+				Pid:       containerJson.State.Pid,
+				Created:   containerJson.Created,
+				StartedAt: containerJson.State.StartedAt,
+				Image:     containerJson.Image,
+				Info:      parseContainerLabels(containerJson.Config.Labels),
 			}
 
 			if !contains(runningContainers, c) {
@@ -97,10 +125,14 @@ func main() {
 				}
 				// write csv to file
 				file, err := os.OpenFile(resultFile, os.O_RDWR|os.O_CREATE, os.ModePerm)
+				// file, err := os.OpenFile(resultFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModePerm)
 				if err != nil {
-					panic(err)
+					fmt.Println("Error opening file: ", err)
+					continue
 				}
 				file.WriteString(csvContents)
+				//file.WriteString(csvContent)
+				fmt.Printf("New pod written: %s  Container: %s\n", c.Info.PodName, c.Info.ContainerName)
 			}
 			// wait 5 seconds
 			time.Sleep(5 * time.Second)
